51a2924ef4b9fea2734daafbc27ed11841060bdc
Senem Soy <ssoy23@gmail.com>
2020-10-06 16:45:20 -0700

    base requirements complete except base stats

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ const App = () => {
       try {
         const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}` //gathering an individual pokemon we search for
         const res = await axios.get(url)
+        toArray.push(res.data);
+        setPokemonData(toArray);
         console.log(res)
       } catch (e) {
         console.log(e)
@@ -22,6 +24,15 @@ const App = () => {
     useEffect(() => {
       getPokemon()
     }, [])
+
+    const handleChange = (e) => {
+      setPokemon(e.target.value.toLowerCase())
+    }
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      getPokemon();
+    }
 
     return (
       <div className="App">
@@ -34,6 +45,46 @@ const App = () => {
             />
           </label>
         </form>
+        {pokemonData.map((data) => {
+          return(
+            <div className="container">
+            <img src={data.sprites["front_default"]} />
+            <div className="divTable">
+              <div className="divTableBody">
+                <div className="divTableRow">
+                  <div className="divTableCell">{data.name}</div>
+                </div>
+                <div className="divTableRow">
+                  <div className="divTableCell">Height</div>
+                  <div className="divTableCell">
+                    {" "}
+                    {Math.round(data.height * 3.9)}"
+                  </div>
+                </div>
+                <div className="divTableRow">
+                  <div className="divTableCell">Weight</div>
+                  <div className="divTableCell">
+                    {" "}
+                    {Math.round(data.weight / 4.3)} lbs
+                  </div>
+                </div>
+                <div className="divTableRow">
+                  <div className="divTableCell">Type</div>
+                  <div className="divTableCell">{data.types && data.types.map((typeObject) => 
+                      typeObject.type.name).join(', ')}
+                  </div>
+                </div>
+                <div className="divTableRow">
+                  <div className="divTableCell">Abilities</div>
+                  <div className="divTableCell">{data.abilities && data.abilities.map((abilityObject) => 
+                      abilityObject.ability.name).join(', ')}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          )
+        })}
       </div>
     );
   
