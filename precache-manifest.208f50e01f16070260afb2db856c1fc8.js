eb4992400708bfe5cd31011996e323dbd1eaa9d9
Senem Soy <ssoy23@gmail.com>
2020-10-07 11:33:56 -0700

    Updates

---
+++ precache-manifest.208f50e01f16070260afb2db856c1fc8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1a066937e804dffaa122946bd1c6afe",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "d3be1d569685d3c8ae9f",
+    "url": "/pokedex/static/css/main.13eebf28.chunk.css"
+  },
+  {
+    "revision": "645bfd6cd97e833c3882",
+    "url": "/pokedex/static/js/2.db0c22f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokedex/static/js/2.db0c22f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3be1d569685d3c8ae9f",
+    "url": "/pokedex/static/js/main.513d2a7e.chunk.js"
+  },
+  {
+    "revision": "5d5b7317cf43a0b8925c",
+    "url": "/pokedex/static/js/runtime-main.2d94522c.js"
+  },
+  {
+    "revision": "b9ea9d22e25bff3e9a7e4c23cb57b68a",
+    "url": "/pokedex/static/media/pokeball.b9ea9d22.png"
+  }
+]);
